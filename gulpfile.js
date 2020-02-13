fbccc66e19eb1e6ba5cc265f5958978bdcd32b6a
megazol <50046183+megazol3000@users.noreply.github.com>
2020-02-13 17:55:35 +0500

    upgrate gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,28 +6,29 @@ const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const del = require('del');
 const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
 
 //Порядок подключения css файлов
 const cssFiles = [
-   './src/css/main.css',
-   './src/css/media.css'
+   './src/css/bootstrap-grid.min.css',
+   './src/css/fonts.css',
+   './src/css/main.css'
 ]
+
 //Порядок подключения js файлов
 const jsFiles = [
-   './src/js/lib.js',
-   './src/js/main.js'
+   './src/js/script.js'
 ]
 
 //Таск на стили CSS
 function styles() {
-   //Шаблон для поиска файлов CSS
    //Всей файлы по шаблону './src/css/**/*.css'
-   return gulp.src(cssFiles)
+   return gulp.src('./src/css/**/*.css')
    //Объединение файлов в один
    .pipe(concat('style.css'))
    //Добавить префиксы
    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
+      overrideBrowserslist: ['last 2 versions'],
       cascade: false
    }))
    //Минификация CSS
@@ -35,15 +36,23 @@ function styles() {
       level: 2
    }))
    //Выходная папка для стилей
-   .pipe(gulp.dest('./build/css'))
+   .pipe(gulp.dest('./build'))
    .pipe(browserSync.stream());
+}
+
+//Таск на сжатие картинок
+function imgmin() {
+   return gulp.src('./src/img/**')
+   .pipe(imagemin({
+      progressive : true
+   }))
+   .pipe(gulp.dest('./build/img/'))
 }
 
 //Таск на скрипты JS
 function scripts() {
-   //Шаблон для поиска файлов JS
    //Всей файлы по шаблону './src/js/**/*.js'
-   return gulp.src(jsFiles)
+   return gulp.src('./src/js/**/*.js')
    //Объединение файлов в один
    .pipe(concat('script.js'))
    //Минификация JS
@@ -66,24 +75,28 @@ function watch() {
       server: {
           baseDir: "./"
       }
-  });
-  //Следить за CSS файлами
-  gulp.watch('./src/css/**/*.css', styles)
-  //Следить за JS файлами
-  gulp.watch('./src/js/**/*.js', scripts)
-  //При изменении HTML запустить синхронизацию
-  gulp.watch("./*.html").on('change', browserSync.reload);
+   });
+   //Следить за CSS файлами
+   gulp.watch('./src/css/**/*.css', styles)
+   //Следить за JS файлами
+   gulp.watch('./src/js/**/*.js', scripts)
+   //Следить за добавлением картинок
+   gulp.watch('./src/img/**', imgmin)
+   //При изменении HTML запустить синхронизацию
+   gulp.watch("./*.html").on('change', browserSync.reload);
 }
 
 //Таск вызывающий функцию styles
 gulp.task('styles', styles);
 //Таск вызывающий функцию scripts
 gulp.task('scripts', scripts);
+//Таск сжатия картинок
+gulp.task('imgmin', imgmin);
 //Таск для очистки папки build
-gulp.task('del', clean);
+gulp.task('clean', clean);
 //Таск для отслеживания изменений
 gulp.task('watch', watch);
-//Таск для удаления файлов в папке build и запуск styles и scripts
-gulp.task('build', gulp.series(clean, gulp.parallel(styles,scripts)));
+//Таск для удаления файлов в папке build и запуск styles, imgmin и scripts
+gulp.task('build', gulp.series(clean, gulp.parallel(styles,scripts,imgmin)));
 //Таск запускает таск build и watch последовательно
 gulp.task('dev', gulp.series('build','watch'));
